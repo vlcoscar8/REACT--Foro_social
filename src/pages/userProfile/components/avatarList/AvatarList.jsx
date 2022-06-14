4bcbd12f40dc7e866d20385641cad45d376d114b
vlcoscar8 <vlcoscar8@gmail.com>
2022-06-14 16:25:37 +0200

    avatar buy and edit flow buttons

--- src/pages/userProfile/components/avatarList/AvatarList.jsx
+++ src/pages/userProfile/components/avatarList/AvatarList.jsx
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
 import { environment } from "../../../../environment/environment";
 
-const AvatarList = ({ userData, userId }) => {
+const AvatarList = ({ userData, userId, handleSetAvatar }) => {
     const [avatarList, setAvatarList] = useState();
+    const [avatarIdClicked, setAvatarIdClicked] = useState();
 
     useEffect(() => {
         fetch(`${environment.API_URL}/avatar/list`)
             .then((res) => res.json())
             .then((data) => setAvatarList(data));
     }, []);
+
+    const avatarClick = (e) => {
+        const avatarId = parseInt(e.target.id);
+        avatarIdClicked === avatarId
+            ? setAvatarIdClicked()
+            : setAvatarIdClicked(avatarId);
+
+        const avatarDetail = avatarList.find((el) => el.id === avatarId);
+        handleSetAvatar(avatarDetail);
+    };
 
     return (
         <div className="avatar-list">
@@ -26,12 +37,20 @@ const AvatarList = ({ userData, userId }) => {
                                         src={el.img}
                                         className="active"
                                         alt="noob"
+                                        id={el.id}
+                                        onClick={avatarClick}
                                     />
                                 ) : (
                                     <img
                                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThOM3Ym808Y2_G5dRm31W9nymXZ5oKi9_wER6wHPOuwQSWZYOm3noqD9fJtA_DZGqDhqs&usqp=CAU"
-                                        className="disable"
+                                        className={
+                                            avatarIdClicked === el.id
+                                                ? "disable-clicked"
+                                                : "disable"
+                                        }
                                         alt="default-avatar"
+                                        id={el.id}
+                                        onClick={avatarClick}
                                     />
                                 )
                             )}
@@ -51,12 +70,20 @@ const AvatarList = ({ userData, userId }) => {
                                         src={el.img}
                                         className="active"
                                         alt="amateur"
+                                        id={el.id}
+                                        onClick={avatarClick}
                                     />
                                 ) : (
                                     <img
                                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThOM3Ym808Y2_G5dRm31W9nymXZ5oKi9_wER6wHPOuwQSWZYOm3noqD9fJtA_DZGqDhqs&usqp=CAU"
-                                        className="disable"
+                                        className={
+                                            avatarIdClicked === el.id
+                                                ? "disable-clicked"
+                                                : "disable"
+                                        }
                                         alt="default-avatar"
+                                        id={el.id}
+                                        onClick={avatarClick}
                                     />
                                 )
                             )}
@@ -76,12 +103,20 @@ const AvatarList = ({ userData, userId }) => {
                                         src={el.img}
                                         className="active"
                                         alt="pro"
+                                        id={el.id}
+                                        onClick={avatarClick}
                                     />
                                 ) : (
                                     <img
                                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThOM3Ym808Y2_G5dRm31W9nymXZ5oKi9_wER6wHPOuwQSWZYOm3noqD9fJtA_DZGqDhqs&usqp=CAU"
-                                        className="disable"
+                                        className={
+                                            avatarIdClicked === el.id
+                                                ? "disable-clicked"
+                                                : "disable"
+                                        }
                                         alt="default-avatar"
+                                        id={el.id}
+                                        onClick={avatarClick}
                                     />
                                 )
                             )}
--- src/pages/userProfile/components/modalAvatar/ModalAvatar.jsx
+++ src/pages/userProfile/components/modalAvatar/ModalAvatar.jsx
@@ -1,15 +1,73 @@
 import { faGem, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import AvatarList from "../avatarList/AvatarList";
-
+import { environment } from "../../../../environment/environment";
 import { AuthStateContext } from "../../../../state/context/authStateContext";
 
 const ModalAvatar = ({ showModal, modal }) => {
     const { userData, userLogged } = useContext(AuthStateContext);
+    const [avatarSelected, setAvatarSelected] = useState();
+    const [error, setError] = useState();
 
     const handleShowModal = () => {
         showModal(false);
+    };
+
+    const handleSetAvatar = (avatarDetail) => {
+        avatarSelected && avatarSelected.id === avatarDetail.id
+            ? setAvatarSelected()
+            : setAvatarSelected(avatarDetail);
+    };
+
+    const handleEditButton = () => {
+        const body = {
+            avatarId: avatarSelected.id,
+        };
+
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+        };
+        avatarSelected &&
+            avatarSelected.users.includes(userLogged.userId) &&
+            fetch(
+                `${environment.API_URL}/user/avatar/${userLogged.userId}`,
+                requestOptions
+            )
+                .then((res) => res.json())
+                .then((data) => console.log(data));
+
+        avatarSelected &&
+            !avatarSelected.users.includes(userLogged.userId) &&
+            setError("You don't have the avatar yet");
+    };
+
+    const handleBuyButton = () => {
+        const body = {
+            avatarProfile: avatarSelected.img,
+            coins: avatarSelected.price,
+        };
+
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+        };
+
+        avatarSelected &&
+            !avatarSelected.users.includes(userLogged.userId) &&
+            fetch(
+                `${environment.API_URL}/user/edit/${userLogged.userId}`,
+                requestOptions
+            )
+                .then((res) => res.json())
+                .then((data) => console.log(data));
+
+        avatarSelected &&
+            avatarSelected.users.includes(userLogged.userId) &&
+            setError("Not money enough or you have already the avatar");
     };
 
     return (
@@ -29,9 +87,42 @@ const ModalAvatar = ({ showModal, modal }) => {
                     <div className="coins">
                         <FontAwesomeIcon icon={faGem} className="icon" />
                         <p>{userData.coins}</p>
+                        <p className="coins__price">
+                            {avatarSelected &&
+                                ` Price: ${avatarSelected.price}`}
+                        </p>
                     </div>
                 </div>
-                <AvatarList userData={userData} userId={userLogged.userId} />
+                <div className="modal-avatar__buttons">
+                    {error && <p>{error}</p>}
+                    <button
+                        className={
+                            avatarSelected &&
+                            avatarSelected.users.includes(userLogged.userId)
+                                ? "modal-avatar__buttons--btn"
+                                : "modal-avatar__buttons--btn disabled"
+                        }
+                        onClick={handleEditButton}
+                    >
+                        Edit
+                    </button>
+                    <button
+                        className={
+                            avatarSelected &&
+                            !avatarSelected.users.includes(userLogged.userId)
+                                ? "modal-avatar__buttons--btn"
+                                : "modal-avatar__buttons--btn disabled"
+                        }
+                        onClick={handleBuyButton}
+                    >
+                        Buy
+                    </button>
+                </div>
+                <AvatarList
+                    userData={userData}
+                    userId={userLogged.userId}
+                    handleSetAvatar={handleSetAvatar}
+                />
             </section>
             <div
                 className={
